96090cc8e5dcc857bffb0527552af66d30e9ba1b
Nargiza Okilova <nokilova@Nargizas-MacBook-Pro.local>
2018-08-08 19:05:49 -0700

    finish JS

---
+++ stalkerSc.js
@@ -0,0 +1,9 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("How old are you?");
+
+alert("Nice to meet you, " + firstName + " " + lastName);
+alert(firstName + " is " + age + " years old.");
+
+console.log(firstName + lastName);
+console.log(age);
